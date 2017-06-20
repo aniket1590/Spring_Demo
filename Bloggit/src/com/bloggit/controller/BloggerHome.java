924d98895471e4ce34592e3ce000c693491fff8f
aniket1590 <akfinaldestination9@gmail.com>
2017-06-20 19:41:14 +0530

    Added Hibernate

--- Bloggit/src/com/bloggit/controller/BloggerHome.java
+++ Bloggit/src/com/bloggit/controller/BloggerHome.java
@@ -3,6 +3,11 @@ package com.bloggit.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import entity.UserMaster;
 /*
  * author: Aniket
  * 
@@ -12,6 +17,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class BloggerHome {
 	@RequestMapping("/welcome")
 	public ModelAndView helloWorld() {
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+ 
+		UserMaster user = new UserMaster("firstuser");
+		session.save(user);
+ 
+		session.getTransaction().commit();
+		session.close();
 		 
 		String message = "<br><div style='text-align:center;'>"
 				+ "<h3>********** Hello </h3> Message **********</div><br><br>";
---
+++ Bloggit/src/entity/UserMaster.java
@@ -0,0 +1,44 @@
+package entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+ 
+@Entity
+public class UserMaster {
+
+	public UserMaster() {
+		 
+	};
+ 
+	public UserMaster(String login) {
+		this.login = login;
+ 
+	};
+ 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+ 
+	private String login;
+ 
+	public Long getId() {
+		return id;
+	}
+ 
+	public void setId(Long id) {
+		this.id = id;
+	}
+ 
+	public String getLogin() {
+		return login;
+	}
+ 
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+}
